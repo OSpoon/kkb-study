8a4f024a7bff7737d264d72cd7d4ba32008888dd
张鑫 <zhangxin@n22.com.cn>
2020-10-29 11:33:20 +0800

    fix🐛

--- jestjs/index.js
+++ jestjs/index.js
@@ -2,3 +2,19 @@ function sum(a, b) {
   return a + b;
 }
 module.exports = sum;
+
+class Person {}
+
+let person = new Person();
+
+console.log("typeof ", typeof person);
+
+console.log("instanceof ", person instanceof Person);
+
+console.log("constructor ", person.constructor === Person);
+
+let toString = Object.prototype.toString;
+console.log(
+  "Object.prototype.toString.call ",
+  toString.call(person) === "[object Object]"
+);
